bf150ea9d19e8bc1cec15a0566378d19c3f3380f
Marcin Dylong <marcindylong@gmail.com>
2020-03-30 21:14:25 +0200

    Formularz logowania

---
+++ fantasy_pl/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core.validators import EmailValidator, URLValidator
+from django.contrib.auth.models import User
+from django.core.validators import ValidationError
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='User name', widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+
+
+    # myfield = forms.CharField(widget=forms.TextInput(attrs={'class' : 'myfieldclass'}))
--- fantasy_pl/views.py
+++ fantasy_pl/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import LoginForm
 
 
 class IndexView(View):
 
     def get(self, request):
+        # user =
         ctx = {'title': ''}
         return render(request, "components/index.html", ctx)
 
@@ -51,11 +54,32 @@ class IconsView(View):
         return render(request, "components/icons.html", ctx)
 
 
+# class LoginView(View):
+#
+#     def get(self, request):
+#         ctx = {'title': "Log in"}
+#         return render(request, "components/login.html", ctx)
+
+
 class LoginView(View):
 
     def get(self, request):
-        ctx = {'title': "Log in"}
-        return render(request, "components/login.html", ctx)
+        ctx = {'title': "Log in", 'form': LoginForm()}
+        return render(request, 'components/login.html', ctx)
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/index')
+            else:
+                return render(request, 'components/login.html', {'form': LoginForm(), 'unsuccessful': True})
+
+        return redirect('')
 
 
 class MorrisView(View):
