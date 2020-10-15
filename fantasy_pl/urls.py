cdf9e8b20dbafa99ae8a2c0f11dff2174d220b98
Marcin Dylong <marcindylong@gmail.com>
2020-10-15 14:40:01 +0200

    Urls small changes

--- fantasy_pl/urls.py
+++ fantasy_pl/urls.py
@@ -1,47 +1,60 @@
 from django.urls import path
 
-from .views import IndexView, LoginView, BlankView, LogoutView, PopulateTeamsView, UpdateTeamsView, PopulatePlayersView, \
-                   UpdatePlayersView, PopulatePositionsView, TeamView, StandingsView, PlayerView, PositionsView, \
-                   StatsView, SearchView, ApiTeamsView, ApiTeamsListView, ApiPlayersListView, ApiPlayersView, \
-                   CreateUserView, ChangetPasswordView, SendMessageView, MessageReceivedView, MessageSentView, \
-                   UserTeamView, ApiUserTeamsListView, ApiUserTeamView, DownloadDataView, GetPlayersHistoryView
+from .views import IndexView, LoginView, BlankView, LogoutView, \
+    PopulateTeamsView, UpdateTeamsView, PopulatePlayersView, \
+    UpdatePlayersView, PopulatePositionsView, TeamView, StandingsView, \
+    PlayerView, PositionsView, StatsView, SearchView, ApiTeamsView, \
+    ApiTeamsListView, ApiPlayersListView, ApiPlayersView, CreateUserView, \
+    ChangetPasswordView, SendMessageView, MessageReceivedView, \
+    MessageSentView, UserTeamView, ApiUserTeamsListView, ApiUserTeamView, \
+    DownloadDataView, GetPlayersHistoryView
 
 app_name = 'fantasy_pl'
 
 urlpatterns = [
+    ## User related
     path('', IndexView.as_view(), name="index"),
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView, name='logout'),
     path('signin/', CreateUserView.as_view(), name='sign_in'),
-    path('change_password/<int:user_id>', ChangetPasswordView.as_view(), name='change_password'),
+    path('change_password/<int:user_id>', ChangetPasswordView.as_view(),
+         name='change_password'),
     path('send_message/', SendMessageView.as_view(), name='send_message'),
-    path('received_message/', MessageReceivedView.as_view(), name='received_message'),
+    path('received_message/', MessageReceivedView.as_view(),
+         name='received_message'),
     path('sent_message/', MessageSentView.as_view(), name='sent_message'),
     path('user_team/', UserTeamView.as_view(), name='user_team'),
     path('blank/', BlankView.as_view(), name="blank"),
-    #  Data download & update
+    ##  Data download & update
     path('download_data/', DownloadDataView.as_view(), name='download_data'),
-    path('populate_teams/', PopulateTeamsView.as_view(), name="populate_teams"),
+    path('populate_teams/', PopulateTeamsView.as_view(),
+         name="populate_teams"),
     path('update_teams/', UpdateTeamsView.as_view(), name="update_teams"),
-    path('populate_players/', PopulatePlayersView.as_view(), name="populate_players"),
-    path('update_players/', UpdatePlayersView.as_view(), name="update_players"),
-    path('populate_positions/', PopulatePositionsView.as_view(), name="positions_players"),
+    path('populate_players/', PopulatePlayersView.as_view(),
+         name="populate_players"),
+    path('update_players/', UpdatePlayersView.as_view(),
+         name="update_players"),
+    path('populate_positions/', PopulatePositionsView.as_view(),
+         name="positions_players"),
     # path('get_ind_player_history/<int:id>', GetIndividualPlayerDataView.as_view(), name="get_ind_player_history"),
-    path('get_players_history/', GetPlayersHistoryView.as_view(), name="get_players_history"),
-    #  Teams, players, stats
+    path('get_players_history/', GetPlayersHistoryView.as_view(),
+         name="get_players_history"),
+    ##  Teams, players, stats
     path('team/<int:id>/<str:sort>', TeamView.as_view(), name='team'),
     path('standings/', StandingsView.as_view(), name='standings'),
     path('player/<int:id>/', PlayerView.as_view(), name='player'),
-    path('positions/<str:pos>/<str:sort>', PositionsView.as_view(), name='positions'),
+    path('positions/<str:pos>/<str:sort>', PositionsView.as_view(),
+         name='positions'),
     path('stats/', StatsView.as_view(), name='stats'),
     path('search/', SearchView.as_view(), name='search'),
-    # API
+    ## API
     path('api/teams', ApiTeamsListView.as_view(), name='api_teams'),
     path('api/teams/<int:pk>', ApiTeamsView.as_view(), name='api_team_detail'),
     path('api/players', ApiPlayersListView.as_view(), name='api_players'),
-    path('api/players/<int:pk>', ApiPlayersView.as_view(), name='api_player_detail'),
-    path('api/userteams', ApiUserTeamsListView.as_view(), name='api_user_teams'),
-    path('api/userteams/<int:pk>', ApiUserTeamView.as_view(), name='api_userteam'),
+    path('api/players/<int:pk>', ApiPlayersView.as_view(),
+         name='api_player_detail'),
+    path('api/userteams', ApiUserTeamsListView.as_view(),
+         name='api_user_teams'),
+    path('api/userteams/<int:pk>', ApiUserTeamView.as_view(),
+         name='api_userteam'),
 ]
-
-
