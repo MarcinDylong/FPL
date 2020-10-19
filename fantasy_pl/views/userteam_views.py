84faa3fec81056cf781a993bbae8e5bf747ede20
Marcin Dylong <marcindylong@gmail.com>
2020-10-19 16:56:27 +0200

    User team view updates

--- fantasy_pl/views/userteam_views.py
+++ fantasy_pl/views/userteam_views.py
@@ -9,60 +9,37 @@ from fantasy_pl.models import UserTeam
 class UserTeamView(View):
 
     def team_overall(self, uteam, ctx):
-        luteam = [uteam.gkp, uteam.def1, uteam.def2, uteam.def3, uteam.def4,
-                  uteam.mdf1, uteam.mdf2, uteam.mdf3, uteam.mdf4, uteam.fwd1,
-                  uteam.fwd2, uteam.gkpb, uteam.defb, uteam.mdfb, uteam.fwdb]
+        luteam = [uteam.gkp, uteam.gkpb, uteam.def1, uteam.def2, uteam.def3,
+                  uteam.def4, uteam.defb, uteam.mdf1, uteam.mdf2, uteam.mdf3,
+                  uteam.mdf4, uteam.mdfb, uteam.fwd1, uteam.fwd2, uteam.fwdb]
         ctx['uteam'] = luteam
 
-        overall_cost = 0
-        for u in luteam:
-            overall_cost += u.now_cost
-        ctx['overall_cost'] = round(overall_cost, 1)
+        overall_cost = sum([u.now_cost for u in luteam])
+        ctx['overall_cost'] = uteam.overall_cost = round(overall_cost, 1)
         if overall_cost > 100:
             ctx['overpaid'] = round(overall_cost - 100, 2)
-        uteam.cost = round(overall_cost, 1)
 
-        points_per_game = 0
-        for u in luteam:
-            points_per_game += u.points_per_game
-        ctx['ppg'] = round(points_per_game, 1)
-        uteam.points_per_game = round(points_per_game, 1)
+        ppg = sum([u.points_per_game for u in luteam])
+        ctx['ppg'] = uteam.ppg = round(ppg, 1)
 
-        influence = 0
-        for u in luteam:
-            influence += u.influence
-        ctx['influence'] = round(influence, 1)
-        uteam.influence = round(influence, 1)
+        influence = sum([u.influence for u in luteam])
+        ctx['influence'] = uteam.influence = round(influence, 1)
 
-        creativity = 0
-        for u in luteam:
-            creativity += u.creativity
-        ctx['creativity'] = round(creativity, 1)
-        uteam.creativity = round(creativity, 1)
+        creativity = sum([u.creativity for u in luteam])
+        ctx['creativity'] = uteam.creativity = round(creativity, 1)
 
-        threat = 0
-        for u in luteam:
-            threat += u.threat
-        ctx['threat'] = round(threat, 1)
-        uteam.threat = round(threat, 1)
+        threat = sum([u.threat for u in luteam])
+        ctx['threat'] = uteam.threat = round(threat, 1)
 
-        dreamteam = 0
-        for u in luteam:
-            dreamteam += u.dreamteam_count
-        ctx['dreamteam'] = round(dreamteam, 1)
-        uteam.dt_apps = round(dreamteam, 1)
+        dreamteam = sum([u.dreamteam_count for u in luteam])
+        ctx['dreamteam'] = uteam.dt_apps = round(dreamteam, 1)
 
-        total_points = 0
-        for u in luteam:
-            total_points += u.total_points
-        ctx['total_points'] = round(total_points, 1)
-        uteam.total_points = round(total_points, 1)
+        total_points = sum([u.total_points for u in luteam])
+        ctx['total_points'] = uteam.total_points = round(total_points, 1)
 
-        novelty = 0
-        for u in luteam:
-            novelty = novelty + u.selected_by_percent
-        ctx['novelty'] = round(novelty / 15, 1)
-        uteam.novelty = round(novelty / 15, 1)
+        novelty = sum([u.selected_by_percent for u in luteam])
+        ctx['novelty'] = uteam.novelty = round(novelty / 15, 1)
+
         uteam.save()
         return ctx
 
@@ -89,7 +66,6 @@ class UserTeamView(View):
             form.fields['defb'].initial = uteam.defb_id
             form.fields['mdfb'].initial = uteam.mdfb_id
             form.fields['fwdb'].initial = uteam.fwdb_id
-
             self.team_overall(uteam, ctx)
 
         return render(request, 'components/userteam.html', ctx)
@@ -102,19 +78,19 @@ class UserTeamView(View):
             try:
                 userTeam = UserTeam.objects.get(user=request.user)
                 userTeam.gkp  = form.cleaned_data['gkp']
+                userTeam.gkpb = form.cleaned_data['gkpb']
                 userTeam.def1 = form.cleaned_data['def1']
                 userTeam.def2 = form.cleaned_data['def2']
                 userTeam.def3 = form.cleaned_data['def3']
                 userTeam.def4 = form.cleaned_data['def4']
+                userTeam.defb = form.cleaned_data['defb']
                 userTeam.mdf1 = form.cleaned_data['mdf1']
                 userTeam.mdf2 = form.cleaned_data['mdf2']
                 userTeam.mdf3 = form.cleaned_data['mdf3']
                 userTeam.mdf4 = form.cleaned_data['mdf4']
+                userTeam.mdfb = form.cleaned_data['mdfb']
                 userTeam.fwd1 = form.cleaned_data['fwd1']
                 userTeam.fwd2 = form.cleaned_data['fwd2']
-                userTeam.gkpb = form.cleaned_data['gkpb']
-                userTeam.defb = form.cleaned_data['defb']
-                userTeam.mdfb = form.cleaned_data['mdfb']
                 userTeam.fwdb = form.cleaned_data['fwdb']
                 userTeam.save()
                 ctx['success'] = 'Team updated'
@@ -122,19 +98,19 @@ class UserTeamView(View):
                 userTeam = UserTeam()
                 userTeam.user = request.user
                 userTeam.gkp = form.cleaned_data['gkp']
+                userTeam.gkpb = form.cleaned_data['gkpb']
                 userTeam.def1 = form.cleaned_data['def1']
                 userTeam.def2 = form.cleaned_data['def2']
                 userTeam.def3 = form.cleaned_data['def3']
                 userTeam.def4 = form.cleaned_data['def4']
+                userTeam.defb = form.cleaned_data['defb']
                 userTeam.mdf1 = form.cleaned_data['mdf1']
                 userTeam.mdf2 = form.cleaned_data['mdf2']
                 userTeam.mdf3 = form.cleaned_data['mdf3']
                 userTeam.mdf4 = form.cleaned_data['mdf4']
+                userTeam.mdfb = form.cleaned_data['mdfb']
                 userTeam.fwd1 = form.cleaned_data['fwd1']
                 userTeam.fwd2 = form.cleaned_data['fwd2']
-                userTeam.gkpb = form.cleaned_data['gkpb']
-                userTeam.defb = form.cleaned_data['defb']
-                userTeam.mdfb = form.cleaned_data['mdfb']
                 userTeam.fwdb = form.cleaned_data['fwdb']
                 userTeam.save()
                 ctx['success'] = 'Team created'
