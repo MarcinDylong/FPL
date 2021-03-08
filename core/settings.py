f0c7ff695632b22706d63b2b00f776e3b7f2bb4b
Marcin Dylong <marcindylong@gmail.com>
2021-03-08 13:51:18 +0100

    settings

--- core/settings.py
+++ core/settings.py
@@ -129,9 +129,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 APPEND_SLASH = True
 
 # REST FRAMEWORK
