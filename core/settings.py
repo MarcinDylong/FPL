4d801fcf68d2055fa443c97deaa99069620c5c86
Marcin Dylong <marcindylong@gmail.com>
2021-03-08 16:08:49 +0100

    fixes

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 APPEND_SLASH = True
 
 # Extra places for collectstatic to find static files.
